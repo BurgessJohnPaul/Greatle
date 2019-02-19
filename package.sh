0403b6f00e08ca88381733511c389ad875043659
Nic Siebenaller <n.7aller@gmail.com>
2019-02-19 17:01:45 -0500

    Script has LF and not CRLF ends

--- package.sh
+++ package.sh
@@ -1,1 +1,8 @@
-#!/bin/bashmkdir packagecd packagepip3 install -r ../requirements.txt --target .zip -r9 ../function.zip .cd ../zip -g function.zip dynamo_helper.pyzip -g function.zip greatle.py
+#!/bin/bash
+mkdir package
+cd package
+pip3 install -r ../requirements.txt --target .
+zip -r9 ../function.zip .
+cd ../
+zip -g function.zip dynamo_helper.py
+zip -g function.zip greatle.py

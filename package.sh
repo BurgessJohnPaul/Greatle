9e6b02fce2513449774aebb58771fbd85f8576ab
Nic Siebenaller <n.7aller@gmail.com>
2019-02-19 16:29:00 -0500

    Added package script for zipping up Lambda function and dependencies

---
+++ package.sh
@@ -0,0 +1,1 @@
+#!/bin/bashmkdir packagecd packagepip3 install -r ../requirements.txt --target .zip -r9 ../function.zip .cd ../zip -g function.zip dynamo_helper.pyzip -g function.zip greatle.py

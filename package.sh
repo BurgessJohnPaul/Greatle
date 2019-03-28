dda98776614e8719378bcd1cb6d505d46364478b
Ethan Wolfe <ethanrw19@gmail.com>
2019-03-28 01:28:44 -0400

    Update package.sh

--- package.sh
+++ package.sh
@@ -5,7 +5,7 @@ pcregrep -M 'BEGIN(.|\n)*?END' ../requirements.txt > ../lambda_reqs.txt
 pip3 install -r ../lambda_reqs.txt --target .
 zip -r9 ../function.zip .
 cd ../
-zip -g function.zip tf_similarity.py
+zip -g function.zip similarity_helper.py
 zip -g function.zip dynamo_helper.py
 zip -g function.zip language_helper.py
 zip -g function.zip goal_helper.py

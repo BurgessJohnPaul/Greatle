4472528c99746360397c0edebc913de1613a913d
burgessjohnpaul <burgess.265@osu.edu>
2019-04-04 13:54:04 -0400

    == to is

--- speech_helper.py
+++ speech_helper.py
@@ -3,7 +3,7 @@ import dynamo_helper
 
 
 def build_response(handler_input, card_title, card_text, speech_text, end_session=False):
-    if handler_input.attributes_manager.session_attributes["drunk_mode_state"] is "True":
+    if handler_input.attributes_manager.session_attributes["drunk_mode_state"] == "True":
         speech_text = '<prosody rate="x-slow"><emphasis level="strong">' + speech_text + '</emphasis></prosody>'
 
     handler_input.response_builder.speak(speech_text).set_card(
@@ -23,7 +23,7 @@ def get_drunk_mode_state(user_id):
     if 'Item' in response and 'drunk_mode_state' in response['Item']:
         print("Drunk Mode State: " + response['Item']['drunk_mode_state'])
         print("Drunk Mode State Type: ", type(response['Item']['drunk_mode_state']))
-        print("Drunk Mode is True: ", response['Item']['drunk_mode_state'] is "True")
-        return response['Item']['drunk_mode_state'] is "True"
+        print("Drunk Mode is True: ", response['Item']['drunk_mode_state'] == "True")
+        return response['Item']['drunk_mode_state'] == "True"
     else:
         return False

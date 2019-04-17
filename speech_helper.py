3678deed5d6f85577f19951c12ca00064862085a
Nic Siebenaller <n.7aller@gmail.com>
2019-04-16 22:41:32 -0400

    Maybe memes work now

--- speech_helper.py
+++ speech_helper.py
@@ -1,3 +1,4 @@
+from ask_sdk_model import ui
 from ask_sdk_model.interfaces.display import RenderTemplateDirective, BodyTemplate2, BackButtonBehavior, ImageInstance, Image
 from ask_sdk_model.ui import SimpleCard, StandardCard
 import dynamo_helper
@@ -7,20 +8,35 @@ def build_response(handler_input, card_title, card_text, speech_text, card_image
     if handler_input.attributes_manager.session_attributes["drunk_mode_state"]:
         speech_text = '<prosody rate="x-slow"><emphasis level="strong">' + speech_text + '</emphasis></prosody>'
 
-    if card_image_url:
-        # card = StandardCard(card_title, card_text, ui.Image(card_image_url, card_image_url))
-        img = Image(sources=[ImageInstance(url=card_image_url)])
+    if card_image_url and supports_display(handler_input):
+        card = StandardCard(card_title, card_text, ui.Image(card_image_url, card_image_url))
+        img = Image('Meme', [ImageInstance(url=card_image_url)])
         directive = RenderTemplateDirective(
                     BodyTemplate2(
                         back_button=BackButtonBehavior.HIDDEN,
                         image=img, title=card_title,
                         text_content=card_text))
-        handler_input.response_builder.speak(speech_text).add_directive(directive).set_should_end_session(end_session)
+        handler_input.response_builder.speak(speech_text).set_card(card).add_directive(directive).set_should_end_session(end_session)
     else:
         card = SimpleCard(card_title, card_text)
         handler_input.response_builder.speak(speech_text).set_card(card).set_should_end_session(end_session)
 
     return handler_input.response_builder.response
+
+
+def supports_display(handler_input):
+    # type: (HandlerInput) -> bool
+    """Check if display is supported by the skill."""
+    try:
+        if hasattr(
+                handler_input.request_envelope.context.system.device.
+                        supported_interfaces, 'display'):
+            return (
+                    handler_input.request_envelope.context.system.device.
+                    supported_interfaces.display is not None)
+    except:
+        return False
+
 
 
 def set_drunk_mode(user_id, handler_input, state):

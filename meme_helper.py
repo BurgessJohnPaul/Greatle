7a76158cb04daf47dde6e617e2d629fa4780a831
Nic Siebenaller <n.7aller@gmail.com>
2019-04-16 21:54:39 -0400

    Create directive instead of card when there is an image

--- meme_helper.py
+++ meme_helper.py
@@ -5,13 +5,13 @@ import os
 reddit = praw.Reddit(client_id=os.environ['ID'],
                      client_secret=os.environ['SECRET'],
                      user_agent='Henry by Greatle')
-checkWords = ['i.imgur.com', 'jpg', 'png', 'jpeg']
+checkWords = ['jpg', 'png', 'jpeg']
 
 def get_memes(sub):
-    return [(submission.title, submission.url) for submission in reddit.subreddit(sub).hot(limit=25) if any(string in submission.url for string in checkWords)]
+    return [(submission.title, submission.url, submission.author.name) for submission in reddit.subreddit(sub).top(limit=25) if any(string in submission.url for string in checkWords)]
 
 def get_meme(sub='HistoryMemes'):
     memes = get_memes(sub)
     return memes[random.randint(0, len(memes) - 1)]
 
-print(get_meme())
+# print(get_meme())

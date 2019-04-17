e2cdcece9969efb9e38c496498beb2579586c352
gleske3 <ryangleske@gmail.com>
2019-04-17 14:36:09 -0400

    Add method to get threshold happiness and sadness

--- sentiment_helper.py
+++ sentiment_helper.py
@@ -6,6 +6,16 @@ tone_analyzer = ToneAnalyzerV3(
     url='https://gateway-wdc.watsonplatform.net/tone-analyzer/api'
 )
 
+HAPPY = "HAPPY"
+SLIGHTLY_HAPPY = "SLIGHTLY_HAPPY"
+NEUTRAL = "NEUTRAL"
+SLIGHTLY_SAD = "SLIGHTLY_SAD"
+SAD = "SAD"
+JOY_TONE = 'Joy'
+ANGER_TONE = 'Anger'
+SAD_TONE = 'Sad'
+
+
 
 def get_sentiment_and_degree(text):
     tone_analysis = tone_analyzer.tone(
@@ -27,3 +37,25 @@ def get_sentiment(text):
         sentiment.append(feeling_value[0])
     #Returns a list of just the sentiments detected
     return sentiment
+
+
+def sentiment_with_threshold(text):
+    tone_analysis = tone_analyzer.tone(
+        {'text': text},
+        'application/json'
+    ).get_result()
+    tones = [(str(tone['tone_name']), tone['score']) for tone in tone_analysis['document_tone']['tones']]
+
+    for tone in tones:
+        if tone[0] == JOY_TONE:
+            if tone[1] > .8:
+                return HAPPY
+            else:
+                return SLIGHTLY_HAPPY
+        if tone[0] == SAD_TONE or ANGER_TONE:
+            if tone[1] > .8:
+                return SAD
+            else:
+                return SLIGHTLY_SAD
+
+    return NEUTRAL
